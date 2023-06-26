60d0b33fc71d82f01353fa4db70b51e09f9e8f3a
yoosuf <yoosuf@Yoosuf>
2023-06-26 16:02:39 +0100

    completed task 2

---
+++ __tests__/models.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const { app } = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+beforeEach(() => {
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET/api/topics/", () => {
+  it("200: should return all topics from the database in an array format", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(topics).toBeInstanceOf(Array);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug", expect.any(String));
+          expect(topic).toHaveProperty("description", expect.any(String));
+        });
+      });
+  });
+});
---
+++ errors/errors.js
@@ -0,0 +1,3 @@
+exports.handlePsqlErrors = (err, req, res, next) => {
+    
+}
