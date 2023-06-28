5013390bd6270bc6f3ebd0887802caa3e559b024
yoosuf <yoosuf@Yoosuf>
2023-06-28 11:40:47 +0100

    completed task 4

--- __tests__/models.test.js
+++ __tests__/models.test.js
@@ -3,7 +3,7 @@ const { app } = require("../app");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
-const endpointsData = require('../endpoints.json')
+const endpointsData = require("../endpoints.json");
 
 beforeEach(() => {
   return seed(data);
@@ -12,6 +12,17 @@ beforeEach(() => {
 afterAll(() => {
   return db.end();
 });
+
+describe('ALL non-existent paths', () => {
+  it('404: return a custom error message when the path is not found', () => {
+    return request(app)
+    .get('/api/nonexistentpath')
+    .expect(404)
+    .then(({ body }) => {
+      expect(body.msg).toBe('Not found')
+    })
+  })
+})
 
 describe("GET/api/topics/", () => {
   it("200: should return all topics from the database in an array format", () => {
@@ -35,7 +46,36 @@ describe("GET/api/", () => {
       .get("/api")
       .expect(200)
       .then(({ body }) => {
-        expect(body).toEqual(endpointsData)
+        expect(body).toEqual(endpointsData);
       });
   });
 });
+
+describe("GET /api/articles/:articles_id", () => {
+  it("200: returns an article based on the article id passed in by user", () => {
+    return request(app)
+      .get("/api/articles/3")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toMatchObject({
+          article_id: 3,
+          title: "Eight pug gifs that remind me of mitch",
+          topic: "mitch",
+          author: "icellusedkars",
+          body: "some gifs",
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  it('404: returns a custom error message if article id does not exist', () => {
+    return request(app)
+    .get('/api/articles/20')
+    .expect(404)
+    .then(({body}) => {
+      expect(body.msg).toBe('Not found')
+    })
+  })
+  
+});
+
--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express')
-const { getAllTopics, getAllEndpoints } = require('./controllers/topics.controller')
+const { getAllTopics, getAllEndpoints, getArticle } = require('./controllers/topics.controller')
+const { handleCustomErrors } = require('./errors/errors')
 
 const app = express()
 
 app.get('/api/topics', getAllTopics)
 
 app.get('/api', getAllEndpoints)
+
+app.get('/api/articles/:article_id', getArticle)
+
+app.all('*', (_, res) => {
+  res.status(404).send({ msg: 'Not found'})
+})
+
+app.use(handleCustomErrors)
+
 
 module.exports = {app}
 
